14336d9fe027d9cb21045a8ed860db969f4de640
DixonLemus3502 <Thewarriorlink98@gmail.com>
2019-05-22 12:31:12 -0600

    En Desarrollacion

---
+++ script.js
@@ -0,0 +1,10 @@
+var pokemon= angular.module("pokemon",[])
+
+pokemon.controller("pokedex", function ($scope, $rootScope, $http){
+	$http({
+		method: "GET",
+		url: "https://pokeapi.co/api/v2/pokemon/4"
+	}).then(function callbackSuccess (y){
+		console.log(y)
+	})
+})
